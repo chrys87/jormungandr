3ce84033347203f87acba1b8b67c7b4e35855a53
Storm Dragon <stormdragon2976@gmail.com>
2018-03-19 10:54:58 -0400

    my attmept at creating a settings reader.

---
+++ src/settings_manager.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env/python3
+
+import imp, os, sys
+
+# Read settings file
+
+def read_settings(settingsFile):
+    global settings
+    s = open(settingsFile)
+    settings = imp.load_source('settings', '', s)
+    s.close()
+
+
+#check for settings file
+if os.path.isfile("/etc/jormungandr/settings/settings.conf"):
+    settingsFile = "/etc/jormungandr/settings/settings.conf"
+# This should load the settings file in the git repo's settings directory, but not sure how to make it do it yet.
+elif os.path.isfile("../settings/settings.conf"):
+    pass
+# There is no settings file, so error out
+else:
+    sys.exit('no settings file found, exiting.')
+
+read_settings(settingsFile)
