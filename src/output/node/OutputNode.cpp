bf4712653a27d9c28f74e14b60812a55cd8d026c
chrys <chrys@linux-a11y.org>
2019-01-16 00:29:32 +0100

    create initial connection handling for network node

--- src/output/node/OutputNode.cpp
+++ src/output/node/OutputNode.cpp
@@ -13,4 +13,11 @@ int main()
     Node n = Node();
     //Node n = Node("127.0.0.1", 22447);
     n.connect();
+    string lineOut;
+    string lineIn;    
+    cin >> lineIn;
+    n.sendLine(lineIn);
+    n.getLine(lineOut);
+    cout << "echo: " << lineOut << endl;
+    
 }
